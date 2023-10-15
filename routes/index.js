4a7460a2d35a8a185ca4d77952a936717750cceb
Elena Ustimenko <ystimenko.e@mail.ru>
2023-10-15 12:02:09 +0300

    registration and authotization

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,23 @@
 const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
-const { NotFoundError } = require('../errors/errors');
+// const { NotFoundError } = require('../errors/errors');
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
 
-router.all('*', (req, res) => {
-  res.status(NotFoundError).send({ message: 'Неверный адрес запроса' });
+router.all('*', (err, req, res, next) => {
+  const { statusCode = 404, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 404
+        ? 'Неверный адрес запроса'
+        : message,
+    });
+  next();
+  // res.status(NotFoundError).send({ message: 'Неверный адрес запроса' });
+  // next(NotFoundError).send({ message: 'Неверный адрес запроса' });
 });
 
 module.exports = router;
