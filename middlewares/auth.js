d60e1bfa59559c6ac247c1fc8c4efb4be6d85c67
Elena Ustimenko <ystimenko.e@mail.ru>
2023-10-19 20:01:33 +0300

    registration and authorization

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,9 +4,27 @@ const { JWT_SECRET, NODE_ENV } = process.env;
 const AuthorizationError = require('../errors/AuthorizationError');
 
 function auth(req, res, next) {
+  let payload;
+  try {
+    const { authorization } = req.headers;
+
+    if ((authorization && authorization.startsWith('Bearer '))) {
+      const token = authorization.replace('Bearer ', '');
+      payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
+      req.user = payload;
+      next();
+    } else {
+      next(new AuthorizationError('Необходима авторизация'));
+    }
+  } catch (error) {
+    next(new AuthorizationError('С токеном что-то не так'));
+  }
+}
+
+/* function auth(req, res, next) {
   const { authorization } = req.headers;
 
-  if (!authorization || authorization.startsWith('Bearer ')) {
+  if (!authorization && authorization.startsWith('Bearer ')) {
     return next(new AuthorizationError('Необходима авторизация'));
   }
 
@@ -21,6 +39,6 @@ function auth(req, res, next) {
   }
   req.user = payload; // записываем  payload в объект запроса
   return next(); // пропускаем запрос дальше
-}
+} */
 
 module.exports = { auth };
